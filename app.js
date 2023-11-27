1aff02bc11a7135417712bd2fa0ebc4dcdd16880
tyler-Wiz <tylertooxclusive@gmail.com>
2023-11-27 13:20:40 +0000

    API Documentation With Swagger Open API

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const Artist = require("./src/routes/artist");
 const albumRouter = require("./src/routes/album");
 const Songs = require("./src/routes/songs");
 const expressConfig = require("./config/express");
+const SwaggerConfig = require("./config/swagger");
 
 const app = express();
 
 // Apply express configurations
 expressConfig(app);
+
+// Apply Swagger configurations
+SwaggerConfig(app);
 
 app.get("", (req, res) => {
   res.status(200);
@@ -22,11 +26,11 @@ app.use("/api/songs", Songs);
 app.use("/api/albums", albumRouter);
 
 // Error Handler -----------
-// app.use((error, req, res, next) => {
-//   res.status(error.status).json({
-//     errorMessage: error.message,
-//     status: error.status,
-//   });
-// });
+app.use((error, req, res, next) => {
+  res.status(error.status).json({
+    errorMessage: error.message,
+    status: error.status,
+  });
+});
 
 module.exports = app;
